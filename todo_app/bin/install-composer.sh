84cf20f8b059321d1a5503176b0587bd4c87a0dd
ryou2307 <ryou.tr2307@icloud.com>
2024-06-19 18:01:55 +0900

    Add Render configuration and fix composer install script

--- todo_app/bin/install-composer.sh
+++ todo_app/bin/install-composer.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
+# Ensure PHP is installed
+if ! command -v php > /dev/null; then
+  echo "PHP is not installed. Installing PHP..."
+  apt-get update && apt-get install -y php-cli
+fi
+
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-then
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]; then
     >&2 echo 'ERROR: Invalid installer signature'
     rm composer-setup.php
     exit 1
 fi
 
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php composer-setup.php --quiet
 RESULT=$?
 rm composer-setup.php
 exit $RESULT
